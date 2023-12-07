b7620eb25067a77c80fe09431177aec52bf222e9
LilianCUSUMANO <145117390+spritelaboisson@users.noreply.github.com>
2023-12-07 21:57:37 +0100

    push1pourjoris

--- 404/main.js
+++ 404/main.js
@@ -2,15 +2,22 @@
 
 const titre = document.querySelector(".titre")
 
-
-const BLOCK_SIZE = 30;
+const BLOCK_SIZE = 10
 const score = document.querySelector("#score")
 score.innerHTML=45
 
 
-var canvas = document.querySelector("#tetris");
-var ctx = canvas.getContext("2d");
+function drawPiece(piece) {
+    var canvas = document.querySelector("#tetris");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "green";
+    for (let i = 0; i < piece.length; i++) {
+        ctx.fillRect(piece[i][0], piece[i][1], BLOCK_SIZE, BLOCK_SIZE);
+        ctx.strokeRect(piece[i][0], piece[i][1], BLOCK_SIZE, BLOCK_SIZE);
+    }
+}
 
+let piece_T = [[10,10],[20,10],[20,0],[30,10]]
+drawPiece(piece_T)
 
-ctx.fillStyle = "green";
-ctx.fillRect(x, y, largeur, hauteur);
+  
