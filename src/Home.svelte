df9b29004f08ca7037f62dd8463f9a3abcdf50de
Mathys Aubert <100727916+MagicsssS@users.noreply.github.com>
2023-12-08 05:31:40 +0100

    We can buy

--- src/Home.svelte
+++ src/Home.svelte
@@ -1,21 +1,37 @@
 <script>
     import EventModal from "./EventModal.svelte";
+    import Game from '../model.js';
 
     let showModal = false;
 
-    let energy = 0.5;
-    let carbon = 0.5;
-    //const energy = getEnergy();
-    function test() {
-        carbon = Math.random();
-        energy = Math.random();
-    }
+    var events = []
+    events.push(new Game.Event("Bombe Nucléaire", "", new Game.Effect(-0.01, -0.01), new Game.Effect(0.01, 0.01)))
 
-    setInterval(test, 2000);
+
+    var upgrades = []
+    upgrades.push(new Game.Upgrade("Usine de Traitement de déchets", 0.7, new Game.Effect(0.2, -0.1)))
+    upgrades.push(new Game.Upgrade("Agriculture bio", 0.6, new Game.Effect(0.2, -0.1)))
+
+
+    /*upgrades.push(new Upgrade("Désinstallation Climatisation", 0.3, new Effect(0.1, -0.2)))
+    upgrades.push(new Upgrade("Centrale Nucléaire", 0.8, new Effect(0.6, -0.3)))
+    upgrades.push(new Upgrade("Barrage", 0.4, new Effect(0.4, -0.5)))
+    upgrades.push(new Upgrade("Eolienne", 0.5, new Effect(0.3, -0.7)))
+    upgrades.push(new Upgrade("Bioénergie", 0.3, new Effect(0.3, -0.7)))*/
+
+
+    var jeu = new Game(100, 1, upgrades, events, new Game.Effect(0.5, 1), showEvent, () => {}, 1000)
+
+
+    let energy = jeu.energy;
+    let carbon = jeu.carbon;
+
+    jeu.start()
+
 
     function showEvent() {
         showModal = true;
-        //pauseGame();
+        jeu.pause();
     }
 </script>
 
@@ -24,7 +40,7 @@
         showEvent();
     }}
 >
-    show modal
+    Show modal
 </button>
 
 <EventModal bind:showModal {...{ rejectButton: "Non", acceptButton: "Oui" }}>
@@ -36,25 +52,14 @@
 </EventModal>
 
 <div class="flex flex-row">
-    <div class="w-2/12 h-screen bg-gray-200 grid place-items-center p-16">
-        <div class="w-1/2 bg-blue-300 box-border h-24 w-24">
-            <img src="" />Yo
-        </div>
-        <div class="w-1/2 bg-blue-300 box-border h-24 w-24">
-            <img src="" />Yo
-        </div>
-        <div class="w-1/2 bg-blue-300 box-border h-24 w-24">
-            <img src="" />Yo
-        </div>
-        <div class="w-1/2 bg-blue-300 box-border h-24 w-24">
-            <img src="" />Yo
-        </div>
-        <div class="w-1/2 bg-blue-300 box-border h-24 w-24">
-            <img src="" />Yo
-        </div>
-        <div class="w-1/2 bg-blue-300 box-border h-24 w-24">
-            <img src="" />Yo
-        </div>
+    <div class="w-2/12 h-screen bg-gray-200 grid place-items-center p-14">
+        <div class="w-1/2 bg-white box-border h-18 w-18" on:click|preventDefault|stopPropagation|capture|nonpassive={() => jeu.buy(upgrades[0])}><img src="usine-dechets.jpg" alt="Usine à déchets"/></div>
+        <div class="w-1/2 bg-white box-border h-18 w-18" on:click|preventDefault|stopPropagation|capture|nonpassive={() => jeu.buy(upgrades[1])}><img src="bio-logo.jpg" alt="Bio"/></div>
+        <div class="w-1/2 bg-white box-border h-18 w-18" on:click|preventDefault|stopPropagation|capture|nonpassive={() => jeu.buy(upgrades[2])}><img src="climatisation.png" alt="Climatisation"/></div>
+        <div class="w-1/2 bg-white box-border h-18 w-18" on:click|preventDefault|stopPropagation|capture|nonpassive={() => jeu.buy(upgrades[3])}><img src="centrale-nucleaire.png" alt="Usine à charbon"/></div>
+        <div class="w-1/2 bg-white box-border h-18 w-18" on:click|preventDefault|stopPropagation|capture|nonpassive={() => jeu.buy(upgrades[4])}><img src="barrage.jpg" alt="Barrage"/></div>
+        <div class="w-1/2 bg-white box-border h-18 w-18" on:click|preventDefault|stopPropagation|capture|nonpassive={() => jeu.buy(upgrades[5])}><img src="eolienne logo.avif" alt="Eolienne"/></div>
+        <div class="w-1/2 bg-white box-border h-18 w-18" on:click|preventDefault|stopPropagation|capture|nonpassive={() => jeu.buy(upgrades[6])}><img src="bioenergie.jpg" alt="Bioenergie"/></div>
     </div>
     <div
         class="w-8/12 h-screen bg-[url('/fond-jeu.jpg')]"
