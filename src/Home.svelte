29792f3035d8cd48479b9f06ba9db02e98657209
TommyDIL <147648136+TommyDIL@users.noreply.github.com>
2023-12-08 05:11:03 +0100

    Change project structure to add an error 404 page

---
+++ src/Home.svelte
@@ -0,0 +1,108 @@
+<script>
+    import EventModal from "./EventModal.svelte";
+
+    let showModal = false;
+
+    let energy = 0.5;
+    let carbon = 0.5;
+    //const energy = getEnergy();
+    function test() {
+        carbon = Math.random();
+        energy = Math.random();
+    }
+
+    setInterval(test, 2000);
+
+    function showEvent() {
+        showModal = true;
+        //pauseGame();
+    }
+</script>
+
+<button
+    on:click={() => {
+        showEvent();
+    }}
+>
+    show modal
+</button>
+
+<EventModal bind:showModal {...{ rejectButton: "Non", acceptButton: "Oui" }}>
+    <h2 slot="header">Titre</h2>
+
+    <div class="w-96 min-w-full">
+        <p>Text</p>
+    </div>
+</EventModal>
+
+<div class="flex flex-row">
+    <div class="w-2/12 h-screen bg-gray-200 grid place-items-center p-16">
+        <div class="w-1/2 bg-blue-300 box-border h-24 w-24">
+            <img src="" />Yo
+        </div>
+        <div class="w-1/2 bg-blue-300 box-border h-24 w-24">
+            <img src="" />Yo
+        </div>
+        <div class="w-1/2 bg-blue-300 box-border h-24 w-24">
+            <img src="" />Yo
+        </div>
+        <div class="w-1/2 bg-blue-300 box-border h-24 w-24">
+            <img src="" />Yo
+        </div>
+        <div class="w-1/2 bg-blue-300 box-border h-24 w-24">
+            <img src="" />Yo
+        </div>
+        <div class="w-1/2 bg-blue-300 box-border h-24 w-24">
+            <img src="" />Yo
+        </div>
+    </div>
+    <div
+        class="w-8/12 h-screen bg-[url('/fond-jeu.jpg')]"
+        style="filter: saturate({1 - carbon})"
+    ></div>
+    <div class="w-2/12 h-screen flex flex-row justify-around bg-gray-200">
+        <div class="flex flex-col">
+            <p class="text-2xl text-center">Energie</p>
+            <div
+                class="flex flex-col flex-nowrap justify-end w-5 h-72 bg-gray-200 rounded-full overflow-hidden dark:bg-gray-700"
+                role="progressbar"
+                aria-valuemin="0"
+                aria-valuemax="100"
+            >
+                <div
+                    class="rounded-full overflow-hidden bg-blue-600 bg-blue-500"
+                    style="height: {energy * 100}%"
+                >
+                    <progress id="energy" />
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col">
+            <p class="text-2xl text-center">Carbone</p>
+            <div
+                class="flex flex-col flex-nowrap justify-end w-5 h-72 bg-gray-200 rounded-full overflow-hidden dark:bg-gray-700"
+                role="progressbar"
+                aria-valuemin="0"
+                aria-valuemax="100"
+            >
+                <div
+                    class="rounded-full overflow-hidden bg-blue-600 bg-blue-500"
+                    style="height: {carbon * 100}%"
+                >
+                    <progress id="carbon" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    #energy {
+        display: block;
+        transform: rotate(-90deg);
+    }
+    #carbon {
+        display: block;
+        transform: rotate(-90deg);
+    }
+</style>
