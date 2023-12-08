aa96903f15cb2c9b1baf06cc2352d93219ef6dce
LilianCUSUMANO <145117390+spritelaboisson@users.noreply.github.com>
2023-12-08 05:14:44 +0100

    tetris merge

--- src/NotFound.svelte
+++ src/NotFound.svelte
@@ -1,1 +1,28 @@
-<h1>Test</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.30/vue.global.min.js"></script>
+  <script src="main.js" defer></script>
+</head>
+<body>
+  <h1 class="titre"><span id="s1">404 </span><span id="s2">vous </span><span id="s3">vous </span><span id="s4">êtes</span> <span id="s5">perdu</span> </h1>
+  <h1 class="victoire" hidden><span id="s1">Félicitation, </span><span id="s2">vous </span><span id="s3">avez perdu </span><span id="s4">votre</span> <span id="s5">temps</span> </h1>
+  <a href="index.html" hidden><button >Retour à l'accueil</button></a>
+  <a href="index.html"><button >Abandon...</button></a>
+  <span id="score"> </span>
+  <span id="ligne"> lignes complétés </span>
+  <canvas id="tetris">
+    
+  </canvas>
+  
+
+  
+
+
+  
+</body>
+</html>
